685b010bc793038032d994e450dc5b7045547f7b
Andika Saputra <acyandika2@gmail.com>
2024-10-06 21:41:30 +0700

    new

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -3,10 +3,12 @@ type Props = {
   children: React.ReactNode;
 };
 
+import Image from "next/image";
+
 export default function ImageCard({ imageUrl, children }: Props) {
   return (
     <figure className="container overflow-hidden rounded-base border-2 border-border dark:border-darkBorder bg-main font-base shadow-light dark:shadow-dark">
-      <img className="max-w-full " src={imageUrl} alt="image" />
+      <Image className="max-w-full " src={imageUrl} alt="image" />
       <figcaption className="border-t-2 text-text border-border dark:border-darkBorder p-4">
         {children}
       </figcaption>
