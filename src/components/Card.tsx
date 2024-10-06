23fc30bbba88d66900e87e4d210500ae5191639e
Andika Saputra <acyandika2@gmail.com>
2024-10-06 21:52:19 +0700

    new

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,55 +1,52 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import ImageCard from "./ImageCard";
 
 import { motion } from "framer-motion";
 
 export default function Card() {
+  const itemCard = [
+    {
+      id: 1,
+      img: "/img/hoodie.svg",
+      name: "Hoodies & Sweatshirt",
+      description: "Explore Now!",
+    },
+    {
+      id: 2,
+      img: "/img/parkas.svg",
+      name: "Coats & Parkas",
+      description: "Explore Now!",
+    },
+    {
+      id: 3,
+      img: "/img/tees.png",
+      name: "Tees & T-Shirt",
+      description: "Explore Now!",
+    },
+  ];
   return (
     <>
       <div className="container mx-auto mt-10">
         <h1 className="text-4xl font-bold text-center mb-10">NEW ARRIVALS</h1>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-12 p-10">
-          {/* Card 1 */}
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            className=""
-          >
-            <ImageCard imageUrl="/img/hoodie.svg">
-              <div className="p-4">
-                <h2 className="text-xl font-semibold">Hoodies & Sweatshirt</h2>
-                <p className="mt-2">Explore Now!</p>
-              </div>
-            </ImageCard>
-          </motion.div>
-
-          {/* Card 2 */}
-          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-            <ImageCard imageUrl="/img/parkas.svg">
-              <div className="p-4">
-                <h2 className="text-xl font-semibold">Coats & Parkas</h2>
-                <p className="mt-2">Explore Now!</p>
-              </div>
-            </ImageCard>
-          </motion.div>
-
-          {/* Card 3 */}
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            className=""
-          >
-            <ImageCard imageUrl="/img/tees.png">
-              <div className="p-4">
-                <h2 className="text-xl font-semibold">Tees & T-Shirt</h2>
-                <p className="mt-2">Explore Now!</p>
-              </div>
-            </ImageCard>
-          </motion.div>
+          {itemCard.map((item, index) => (
+            <motion.div
+              key={index}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className=""
+            >
+              <ImageCard imageUrl={item.img}>
+                <div className="p-4">
+                  <h2 className="text-xl font-semibold">{item.name}</h2>
+                  <p className="mt-2">{item.description}</p>
+                </div>
+              </ImageCard>
+            </motion.div>
+          ))}
         </div>
       </div>
 
--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,14 +1,21 @@
+import Image from "next/image";
+
 type Props = {
   imageUrl: string;
   children: React.ReactNode;
 };
 
-import Image from "next/image";
-
 export default function ImageCard({ imageUrl, children }: Props) {
   return (
-    <figure className="container overflow-hidden rounded-base border-2 border-border dark:border-darkBorder bg-main font-base shadow-light dark:shadow-dark">
-      <Image className="max-w-full " src={imageUrl} alt="image" />
+    <figure className="container overflow-hidden rounded-lg border-2 border-border dark:border-darkBorder bg-main font-base shadow-light dark:shadow-dark">
+      <Image
+        className="max-w-full"
+        src={imageUrl}
+        alt="image" // Consider making this more descriptive for better accessibility
+        width={200}
+        height={200}
+        layout="responsive" // Makes the image responsive
+      />
       <figcaption className="border-t-2 text-text border-border dark:border-darkBorder p-4">
         {children}
       </figcaption>
